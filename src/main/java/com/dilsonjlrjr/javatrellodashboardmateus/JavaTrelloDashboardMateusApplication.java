27bb0d57fd24364f025f8eb641cca1d0237b5806
Dilson Rabelo <dilsonjlrjr@gmail.com>
2021-05-17 15:44:05 -0300

    chore(project): Project configuration

--- src/main/java/com/dilsonjlrjr/javatrellodashboardmateus/JavaTrelloDashboardMateusApplication.java
+++ src/main/java/com/dilsonjlrjr/javatrellodashboardmateus/JavaTrelloDashboardMateusApplication.java
@@ -2,8 +2,9 @@ package com.dilsonjlrjr.javatrellodashboardmateus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class JavaTrelloDashboardMateusApplication {
 
     public static void main(String[] args) {
